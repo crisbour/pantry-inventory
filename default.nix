e11fa737a62e99b9c5bc7e1fe6b758b69c813f67
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-09-13 09:08:10 +0100

    Initial repo structure for pantry repository based on previous report

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { }, ... }:
+
+pkgs.stdenv.mkDerivation {
+  name = "pdf";
+  src = ./.;
+  buildInputs = with pkgs; [
+    texliveFull
+  ];
+  buildPhase = ''
+    mkdir -p .cache/latex
+    latexmk -interaction=nonstopmode -auxdir=.cache/latex -pdf main.tex
+  '';
+  installPhase = ''
+    mkdir -p $out
+    cp main.pdf $out/PGR_2025_PhD_First_Year_Review_s2703496.pdf
+  '';
+}
